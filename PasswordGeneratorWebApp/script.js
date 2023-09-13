96e7b0c30ff6db9e62d92a6576268c59900847e5
Sangram <sangram1677@gmail.com>
2023-09-14 02:35:52 +0530

    script updated

--- PasswordGeneratorWebApp/script.js
+++ PasswordGeneratorWebApp/script.js
@@ -10,6 +10,7 @@ const symbolCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
 const generateBtn = document.querySelector(".generateBtn");
 const allCheckbox = document.querySelectorAll("input[type=checkbox");
+const symbols = '!@#$%^&*()_+~\\`|}{[]:;?><,./-='
 
 let password = "";
 let passwordLength = 15;
@@ -27,3 +28,48 @@ function setIndicator(color){
 
     indicator.computedStyleMap.backgroundColor = color;
 }
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+function generateRandomNumber(){
+    return getRndInteger(0,9);
+}
+
+function generateLowerCase() {
+    return String.fromCharCode(getRndInteger(97,123))
+}
+
+function generateUpperCase() {
+     return String.fromCharCode(getRndInteger(65,97))
+}
+
+function generateSymbol() {
+    const randNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrength() {
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+    if (uppercaseCheck.checked) hasUpper = true;
+    if (lowercaseCheck.checked) hasLower = true;
+    if (numbersCheck.checked) hasNum = true;
+    if (symbolsCheck.checked) hasSym = true;
+  
+    if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+      setIndicator("#0f0");
+    } else if (
+      (hasLower || hasUpper) &&
+      (hasNum || hasSym) &&
+      passwordLength >= 6
+    ) {
+      setIndicator("#ff0");
+    } else {
+      setIndicator("#f00");
+    }
+}
+
