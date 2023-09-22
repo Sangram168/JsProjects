91c6dec09598355cec002bf39315ec8b9eaafcc0
Sangram <sangram1677@gmail.com>
2023-09-22 15:58:48 +0530

    script updated

--- PasswordGeneratorWebApp/script.js
+++ PasswordGeneratorWebApp/script.js
@@ -20,18 +20,21 @@ let password = "";
 let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
-
+//ste strength circle color to grey
+setIndicator("#ccc");
 
 //set passwordLength
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
+    const min = inputSlider.min;
+    const max = inputSlider.max;
+    inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%";
 }
-
 
 function setIndicator(color) {
     indicator.style.backgroundColor = color;
-    //shadow - HW
+    indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 
