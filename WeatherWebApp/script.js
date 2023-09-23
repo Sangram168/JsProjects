8377ed05affecd12eafc8bf9b3aec257d475b72d
Sangram <sangram1677@gmail.com>
2023-09-24 01:36:08 +0530

    script updated

---
+++ WeatherWebApp/script.js
@@ -0,0 +1,86 @@
+
+const API_KEY = "522a43f3783ca0cd2b7f91f9b3b98812";
+
+function renderWeatherInfo(data) {
+    let newPara = document.createElement('p');
+    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`;
+    
+    document.body.appendChild(newPara);
+}
+
+async function fetchWeatherDetails(){
+
+    try{
+        let city = "goa";
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    console.log("Weather data:->" , data);
+
+    
+     renderWeatherInfo(data);
+     }
+
+    catch(err){
+        // alert("There is an error")
+    }
+}
+
+async function getCustomWeatherDetails() {
+    try{
+        let latitude = 15.633;
+        let longitude = 18.333;
+    
+        let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?
+                                lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    
+        let data = await result.json();
+    
+        console.log(data);
+    }
+    catch(err) {
+        console.log("Error Found" , err);
+    }
+
+}
+
+function switchTab(clickedTab) {
+
+    apiErrorContainer.classList.remove("active");
+  
+    if (clickedTab !== currentTab) {
+      currentTab.classList.remove("current-tab");
+      currentTab = clickedTab;
+      currentTab.classList.add("current-tab");
+  
+      if (!searchForm.classList.contains("active")) {
+        userInfoContainer.classList.remove("active");
+        grantAccessContainer.classList.remove("active");
+        searchForm.classList.add("active");
+      } 
+      else {
+        searchForm.classList.remove("active");
+        userInfoContainer.classList.remove("active");
+        //getFromSessionStorage();
+      }
+  
+      // console.log("Current Tab", currentTab);
+    }
+  }
+
+  function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        console.log("No geoLocation Support");
+    }
+}
+
+function showPosition(position) {
+    let lat = position.coords.latitude;
+    let longi = position.coords.longitude;
+
+    console.log(lat);
+    console.log(longi);
+}
